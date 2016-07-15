c6a9c46cde740973ce5105a1aa74d5b36f8f71f0
pony <mawei78951@126.com>
2016-07-15 16:03:07 +0800

    slide

---
+++ application/logs/log-2016-07-15.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-07-15 10:02:29 --> Severity: Warning --> chmod(): Operation not permitted /Applications/XAMPP/xamppfiles/htdocs/dudu/system/libraries/Cache/drivers/Cache_file.php 106
+ERROR - 2016-07-15 10:02:29 --> Severity: Warning --> chmod(): Operation not permitted /Applications/XAMPP/xamppfiles/htdocs/dudu/system/libraries/Cache/drivers/Cache_file.php 106
+ERROR - 2016-07-15 10:02:29 --> Severity: Warning --> chmod(): Operation not permitted /Applications/XAMPP/xamppfiles/htdocs/dudu/system/libraries/Cache/drivers/Cache_file.php 106
+ERROR - 2016-07-15 10:02:34 --> 404 Page Not Found: Maincss/index
+ERROR - 2016-07-15 10:02:41 --> 404 Page Not Found: Maincss/index
