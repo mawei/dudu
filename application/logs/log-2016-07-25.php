ecbe0f579b6bf2fdd7492c0c484ea1b879ed2a93
pony <mawei78951@126.com>
2016-07-25 12:30:47 +0800

    a

--- application/logs/log-2016-07-25.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-07-25 06:27:28 --> Severity: Parsing Error --> syntax error, unexpected '}' /Applications/XAMPP/xamppfiles/htdocs/dudu/application/controllers/api/Api.php 399
