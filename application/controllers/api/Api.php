6cb722467b49b479a9d57d04f0800a4fc87a558a
pony <mawei78951@126.com>
2016-08-02 16:12:47 +0800

    a

--- application/controllers/api/Api.php
+++ application/controllers/api/Api.php
@@ -524,7 +524,7 @@ class Api extends Api_Controller {
 	{
 		$driver_id = $this->encrypt->decode ( $this->format_get ( 'driver_id' ), $this->key );
 		$order_id = $this->format_get('order_id');
-		$query_str = " select t1.*,t2.photo as driver_photo,t2.nickname as driver_nickname,t2.telephone as driver_telephone from `t_aci_order` t1 left join `t_aci_driver` t2 on t1.driver_id=t2.driver_id where t1.order_id='{$order_id}' and (t1.driver_id='{$driver_id}' or t1.driver_id IS NULL)";
+		$query_str = " select t1.*,t2.photo as customer_photo,t2.nickname as customer_nickname,t2.telephone as customer_telephone from `t_aci_order` t1 left join `t_aci_customer` t2 on t1.customer_id=t2.customer_id where t1.order_id='{$order_id}' and (t1.driver_id='{$driver_id}' or t1.driver_id IS NULL)";
 		$result = $this->db->query ( $query_str )->result_array ();
 		if(count($result) > 0)
 		{
