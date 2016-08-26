ebf06b38bdceee610aaff4b9e6d532517a2420ee
pony <mawei78951@126.com>
2016-08-26 18:01:22 +0800

    output

--- application/controllers/api/Api.php
+++ application/controllers/api/Api.php
@@ -892,6 +892,56 @@ class Api extends Api_Controller {
 		$this->output_result ( 0, 'success', $query->result_array () );
 	}
 
+	function output_orders_by_customer() 
+	{ 
+		$customer_id = $this->encrypt->decode ( $this->format_get ( 'customer_id' ), $this->key );
+		$status = $this->format_get( 'mail' );
+		$ids = $this->format_get( 'ids');
+		$order_ids = explode(",", $ids);
+
+		$query = $this->db->query("select * from `t_aci_order` WHERE customer_id= {$customer_id} and order_id in {$order_ids}"); 
+		$query = mb_convert_encoding("gb2312", "UTF-8", $query); 
+		if(!$query) 
+			return false; 
+		// Starting the PHPExcel library 
+		$this->load->library('PHPExcel'); 
+		$this->load->library('PHPExcel/IOFactory'); 
+		$objPHPExcel = new PHPExcel(); 
+		$objPHPExcel->getProperties()->setTitle("export")->setDescription("none"); 
+		$objPHPExcel->setActiveSheetIndex(0) 
+		->setCellValue('A1', iconv('gbk', 'utf-8', '中文Hello')) 
+		->setCellValue('B2', 'world!') 
+		->setCellValue('C1', 'Hello'); 
+		// Field names in the first row 
+		$fields = $query->list_fields(); 
+		$col = 0; 
+		foreach ($fields as $field) 
+		{ 
+			$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, 1, $field); 
+			$col++; 
+		} 
+		// Fetching the table data 
+		$row = 2; 
+		foreach($query->result() as $data) 
+		{ 
+			$col = 0; 
+			foreach ($fields as $field) 
+			{ 
+				$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, $row, $data->$field); 
+				$col++; 
+			} 
+			$row++; 
+		} 
+		$objPHPExcel->setActiveSheetIndex(0); 
+		$objWriter = IOFactory::createWriter($objPHPExcel, 'Excel5'); 
+		//发送标题强制用户下载文件 
+		header('Content-Type: application/vnd.ms-excel'); 
+		header('Content-Disposition: attachment;filename="Products_'.date('dMy').'.xls"'); 
+		header('Cache-Control: max-age=0'); 
+		$objWriter->save('php://output'); 
+		$objWriter->save('/'); 
+	}
+
 	function get_order_detail_by_customer()
 	{
 		$customer_id = $this->encrypt->decode ( $this->format_get ( 'customer_id' ), $this->key );
