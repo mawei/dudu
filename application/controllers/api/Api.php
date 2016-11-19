a6251a338759ec88b5112cf6aab82c283c9d57e6
pony <mawei78951@126.com>
2016-11-19 18:05:56 +0800

    a

--- application/controllers/api/Api.php
+++ application/controllers/api/Api.php
@@ -272,13 +272,13 @@ class Api extends Api_Controller {
     		) t4 on t3.order_id=t4.order_id where t3.status='未接单' order by t3.start_time desc
 			";
 		}else{
+			// sqrt(POW((6370693.5 * cos({$latitude} * 0.01745329252) * ({$longitude} * 0.01745329252 - t1.longitude * 0.01745329252)),2) + POW((6370693.5 * ({$latitude} * 0.01745329252 - t1.latitude * 0.01745329252)),2)) as 'distance',
+			// 		sqrt(POW((6370693.5 * cos({$maplatitude} * 0.01745329252) * ({$maplongitude} * 0.01745329252 - t1.longitude * 0.01745329252)),2) + POW((6370693.5 * ({$maplatitude} * 0.01745329252 - t1.latitude * 0.01745329252)),2)) as 'mapdistance',
 			$distance = $distance >= 50000 ? 50000 : $distance;
 			$query_str = "
 			select t4.*,t3.`truck_type`,t3.`truck_size`,t3.start_place,t3.end_place,t3.charge,t3.miles from t_aci_order  t3
 			JOIN(
-				SELECT t1.order_id,
-					sqrt(POW((6370693.5 * cos({$latitude} * 0.01745329252) * ({$longitude} * 0.01745329252 - t1.longitude * 0.01745329252)),2) + POW((6370693.5 * ({$latitude} * 0.01745329252 - t1.latitude * 0.01745329252)),2)) as 'distance',
-					sqrt(POW((6370693.5 * cos({$maplatitude} * 0.01745329252) * ({$maplongitude} * 0.01745329252 - t1.longitude * 0.01745329252)),2) + POW((6370693.5 * ({$maplatitude} * 0.01745329252 - t1.latitude * 0.01745329252)),2)) as 'mapdistance',
+				SELECT t1.order_id,0 as 'distance',0 as 'mapdistance',
 				t1.`latitude` as start_place_latitude,t1.`longitude` as start_place_longitude FROM `t_aci_address`  t1
 				where t1.type='出发地' and t1.latitude between ($maplatitude - 0.1) and ($maplatitude + 0.1) and t1.longitude between ($maplongitude - 0.1) and ($maplongitude + 0.1)
     		) t4 on t3.order_id=t4.order_id where t3.status='未接单' and t4.mapdistance <= $distance order by t4.distance desc
