f664db257113ab109ba5646629ad90846b7a715a
pony <mawei78951@126.com>
2016-12-27 13:12:29 +0800

    a

--- application/thirdparty/sdk/SDKConfig.php
+++ application/thirdparty/sdk/SDKConfig.php
@@ -4,39 +4,39 @@ namespace com\unionpay\acp\sdk;
 // ######(以下配置为PM环境：入网测试环境用，生产环境配置见文档说明)#######
 // 签名证书路径
 //const SDK_SIGN_CERT_PATH =  "../assets/测试环境证书/acp_test_sign.pfx";
-define('SDK_SIGN_CERT_PATH', APPPATH.'thirdparty/assets/测试环境证书/acp_test_sign.pfx');
+define('SDK_SIGN_CERT_PATH', APPPATH.'thirdparty/assets/生产环境证书/cfca.pfx');
 
 // 签名证书密码
-const SDK_SIGN_CERT_PWD = '000000';
+const SDK_SIGN_CERT_PWD = '123456';
 
 // 密码加密证书（这条一般用不到的请随便配）
-define('SDK_ENCRYPT_CERT_PATH', APPPATH.'thirdparty/assets/测试环境证书/acp_test_enc.cer');
+define('SDK_ENCRYPT_CERT_PATH', APPPATH.'thirdparty/assets/生产环境证书/acp_prod_enc.cer');
 
 // 验签证书路径（请配到文件夹，不要配到具体文件）
-define('SDK_VERIFY_CERT_DIR', APPPATH.'thirdparty/assets/测试环境证书/');
+define('SDK_VERIFY_CERT_DIR', APPPATH.'thirdparty/assets/生产环境证书/');
 
 
 
 // 前台请求地址
-const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
+const SDK_FRONT_TRANS_URL = 'https://gateway.95516.com/gateway/api/frontTransReq.do';
 
 // 后台请求地址
-const SDK_BACK_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/backTransReq.do';
+const SDK_BACK_TRANS_URL = 'https://gateway.95516.com/gateway/api/backTransReq.do';
 
 // 批量交易
-const SDK_BATCH_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/batchTrans.do';
+const SDK_BATCH_TRANS_URL = 'https://gateway.95516.com/gateway/api/batchTrans.do';
 
 //单笔查询请求地址
-const SDK_SINGLE_QUERY_URL = 'https://101.231.204.80:5000/gateway/api/queryTrans.do';
+const SDK_SINGLE_QUERY_URL = 'https://gateway.95516.com/gateway/api/queryTrans.do';
 
 //文件传输请求地址
-const SDK_FILE_QUERY_URL = 'https://101.231.204.80:9080/';
+const SDK_FILE_QUERY_URL = 'https://filedownload.95516.com/';
 
 //有卡交易地址
-const SDK_Card_Request_Url = 'https://101.231.204.80:5000/gateway/api/cardTransReq.do';
+const SDK_Card_Request_Url = 'https://gateway.95516.com/gateway/api/cardTransReq.do';
 
 //App交易地址
-const SDK_App_Request_Url = 'https://101.231.204.80:5000/gateway/api/appTransReq.do';
+const SDK_App_Request_Url = 'https://gateway.95516.com/gateway/api/appTransReq.do';
 
 // 前台通知地址 (商户自行配置通知地址)
 const SDK_FRONT_NOTIFY_URL ='http://dudu.yisunbird.com/api/api/pay_back';
